783c649b73c5375f3b57729c155ca15b2c20e0b6
rogerioiwasaki <iwasaki.rogerio@gmail.com>
2018-09-27 16:39:48 -0300

    Projeto criado

---
+++ src/main/java/br/com/notabrasil/app/danfe/DanfeApplication.java
@@ -0,0 +1,12 @@
+package br.com.notabrasil.app.danfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanfeApplication.class, args);
+	}
+}
